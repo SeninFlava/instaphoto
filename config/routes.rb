d96afeda8b6246edeb7d4bcb6113dac0d6d49f6c
Alexander Senin <Senin@MacBook-Pro-Alexander.local>
2016-11-22 09:17:40 +0500

    Links and navigations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Instaphoto::Application.routes.draw do
+  root 'pages#landing'
+  get "pages/landing"
+  get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
