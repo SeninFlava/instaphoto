64872aaff103ae3273d3f824a38404a2808d74eb
Alexander Senin <Senin@MacBook-Pro-Alexander.local>
2016-11-22 08:52:46 +0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instaphoto::Application.config.secret_key_base = '2493fa13fff04b7fa6df4275675129479880c25664dfd34d0a11e4d401a0fdea82f8ad8adfc43da9e48ff53673cb7c32e2633a8ee7a99b8f778aa882e1c7394b'
